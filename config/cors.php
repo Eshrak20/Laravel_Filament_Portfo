2890b5b9edc58a0842a9ece00a62f2d59b3c877a
eshrak20 <uemran2018@gmail.com>
2026-06-27 23:49:10 +0600

    access the origin

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'https://e.veringroup.com/'], // <-- your Vite frontend
+    'allowed_origins' => ['http://localhost:5173', 'https://e.veringroup.com'], // <-- your Vite frontend
 
     'allowed_origins_patterns' => [],
 
